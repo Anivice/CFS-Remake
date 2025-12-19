a1d7de49a611666f738963feaa2edcc4b9051434
anivice <AniviceIves@gmail.com>
2025-12-19 22:15:30 +0800

    basic command handler routine

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,43 @@ bool command_main_entry_point(const std::vector<std::string> & vec)
     if (vec.front() == "help") {
         std::cout << cmdTpTree::command_template_tree.get_help();
     }
+    else if (vec.front() == "help_at") {
+        try {
+            const std::vector help_path(vec.begin() + 1, vec.end());
+            std::ranges::for_each(help_path, [](const auto & v) { std::cout << v << " "; });
+            std::cout << ": " << cmdTpTree::command_template_tree.get_help(help_path) << std::endl;
+        } catch (std::exception & e) {
+            elog(e.what(), "\n");
+        }
+    }
+    else if (vec.front() == "version") {
+        std::cout.write(reinterpret_cast<const char *>(version_text), version_text_len);
+        std::cout << std::endl;
+    }
+    else if (vec.front() == "format") {
+        if (vec.size() < 2) {
+            std::cerr << "format [DISK] <BLOCK SIZE> <LABEL>" << std::endl;
+            return true;
+        }
+
+        const std::string & disk = vec[1];
+        std::string block_size_str = "4096", label;
+
+        if (vec.size() >= 3) {
+            block_size_str = vec[2];
+        }
+
+        if (vec.size() == 4) {
+            label = vec[3];
+        }
+
+        try {
+            const auto block_size = std::strtoull(block_size_str.c_str(), nullptr, 10);
+            cfs::make_cfs(disk, block_size, label);
+        } catch (std::exception & e) {
+            elog(e.what(), "\n");
+        }
+    }
 
     return true;
 }
